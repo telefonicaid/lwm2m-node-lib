d18b33af042902dc3c4f004e2470906b5c1d16b5
Daniel Moran <dmj@tid.es>
2014-10-28 19:40:20 +0100

    FIX Headers in source files

--- test/unit/bootstrap-test.js
+++ test/unit/bootstrap-test.js
@@ -1,5 +1,27 @@
-'use strict';
+/*
+ * Copyright 2014 Telefonica Investigación y Desarrollo, S.A.U
+ *
+ * This file is part of iotagent-lwm2m-lib
+ *
+ * iotagent-lwm2m-lib is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * iotagent-lwm2m-lib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with iotagent-lwm2m-lib.
+ * If not, seehttp://www.gnu.org/licenses/.
+ *
+ * For those usages not covered by the GNU Affero General Public License
+ * please contact with::[contacto@tid.es]
+ */
 
+'use strict';
 
 describe('Client initiated boostrap interface tests', function() {
     describe('When a bootstrapping request comes to the server initiated by the client', function () {
