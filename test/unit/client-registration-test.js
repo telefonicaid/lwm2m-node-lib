938f879e49de1801e4fd88f978a630d78f66bd5b
Daniel Moran <dmj@tid.es>
2014-10-28 14:28:03 +0100

    ADD Device registration routing

--- test/unit/client-registration-test.js
+++ test/unit/client-registration-test.js
@@ -1,8 +1,44 @@
+/*
+ * Copyright 2013 Telefonica Investigación y Desarrollo, S.A.U
+ *
+ * This file is part of iotagent-lwm2m-lib
+ *
+ * iotagent-lwm2m-lib is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * iotagent-lwm2m-lib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with iotagent-lwm2m-lib.
+ * If not, seehttp://www.gnu.org/licenses/.
+ *
+ * For those usages not covered by the GNU Affero General Public License
+ * please contact with::[contacto@tid.es]
+ */
+
 'use strict';
+
+var libLwm2m2 = require('../..'),
+    coap = require('coap'),
+    Readable = require('stream').Readable,
+    should = require('should');
 
 
 describe('Client registration interface tests', function() {
-    describe('When a client registration requests doesn\'t indicate a endpoint name arrives', function () {
+    beforeEach(function (done) {
+        libLwm2m2.start(null, done);
+    });
+
+    afterEach(function(done) {
+        libLwm2m2.stop(done);
+    });
+
+    describe('When a client registration requests doesn\'t indicate a endpoint name arrives', function() {
         it('should fail with a 4.00 Bad Request');
     });
     describe('When a client registration requests doesn\'t indicate a endpoint name arrives', function () {
@@ -12,6 +48,27 @@ describe('Client registration interface tests', function() {
         it('should fail with a 4.00 Bad Request if the request doesn\'t indicate a binding');
     });
     describe('When a correct client registration requests arrives', function () {
-        it('should return a 2.01 Created code');
+        var requestUrl =  {
+                host: 'localhost',
+                port: 5683,
+                method: 'POST',
+                pathname: '/rd',
+                query: 'ep=ROOM001&lt=86400&lwm2m=1.0&b=U'
+            },
+            payload = '</1>, </2>, </3>, </4>, </5>';
+
+        it('should return a 2.01 Created code', function(done) {
+            var req = coap.request(requestUrl),
+                rs = new Readable();
+
+            rs.push(payload);
+            rs.push(null);
+            rs.pipe(req);
+
+            req.on('response', function(res) {
+                res.code.should.equal('2.01');
+                done();
+            });
+        });
     });
 });
