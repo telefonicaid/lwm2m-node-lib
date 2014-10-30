919d8aafb588826d9ed3db66ecd9f6417a75b7b8
Daniel Moran <dmj@tid.es>
2014-10-30 09:44:01 +0100

    ADD Update registration functionalities

--- test/unit/update-registration-test.js
+++ test/unit/update-registration-test.js
@@ -23,10 +23,54 @@
 
 'use strict';
 
+var libLwm2m2 = require('../..'),
+    coap = require('coap'),
+    utils = require('./testUtils'),
+    async = require('async'),
+    should = require('should');
 
 describe('Client update registration interface tests', function() {
-    describe('When a correct cliente registration update request arrives', function() {
-        it('should update the indicated attributes');
-        it('should return a 2.04 Changed code');
+    var deviceLocation;
+
+    function registerHandlers(callback) {
+        libLwm2m2.setHandler('registration', function(endpoint, lifetime, version, binding, innerCb) {
+            innerCb();
+        });
+
+        libLwm2m2.setHandler('updateRegistration', function(object, innerCb) {
+            innerCb();
+        });
+
+        callback();
+    }
+
+    beforeEach(function (done) {
+        async.series([
+            async.apply(libLwm2m2.start, null),
+            registerHandlers,
+            utils.registerClient
+        ], function (error, results) {
+            deviceLocation = results[2];
+            done();
+        });
+    });
+
+    afterEach(function(done) {
+        libLwm2m2.stop(done);
+    });
+
+    describe.only('When a correct cliente registration update request arrives', function() {
+        var updateRequest = {
+            host: 'localhost',
+            port: 5683,
+            method: 'PUT',
+            query: 'lt=86400&b=U'
+        }
+
+        beforeEach(function() {
+            updateRequest.pathname = deviceLocation;
+        });
+
+        it('should return a 2.04 Changed code', utils.checkCode(updateRequest, '', '2.04'));
     });
 });
